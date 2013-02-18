eecffae12db20044c03297cea073e92feaa3e244
lshuai <lshuai@hawk.iit.edu>
2013-02-18 10:09:52 -0600

    RBNode is ready

--- redBlackTree/RBNode.java
+++ redBlackTree/RBNode.java
@@ -1,5 +1,27 @@
 package redBlackTree;
 
 public class RBNode {
-
+	public enum Color{
+		RED, BLACK;
+	}
+	
+	protected RBNode left, right, parent;
+	protected Integer data;
+	Color color;
+	
+	public RBNode(RBNode parent, Integer data){
+		left = new RBNode(this);
+		right = new RBNode(this);
+		this.parent = parent;
+		this.data = data;
+		color = Color.RED;
+	}
+	
+	public RBNode(RBNode parent){
+		left = null;
+		right = null;
+		this.parent = parent;
+		data = null;
+		color = Color.BLACK;
+	}
 }
--- redBlackTree/RedBlackTree.java
+++ redBlackTree/RedBlackTree.java
@@ -1,13 +1,11 @@
 package redBlackTree;
 
 public class RedBlackTree {
-
-	/**
-	 * @param args
-	 */
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
